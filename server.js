71f63dcbbb7e4271f4f336df1f00507a25a5b482
Otávio Pace <otavio.pace@fcamara.com.br>
2017-08-18 09:31:41 -0300

    Extração das rotas e models do server.js

--- server.js
+++ server.js
@@ -1,99 +1,15 @@
 const express = require('express')
 const app = express()
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('pokemons', null, null, {
-	dialect: 'sqlite'
-})
 const request = require('request-promise')
 const applyMiddleware = require('./middleware')
+const syncModels = require('./models')
+const setRoutes = require('./modules')
+const PORT = 3000
+syncModels()
 
 applyMiddleware(app)
+setRoutes(app)
 
-app.listen(3000, function () {
-	console.log('Listening on http://localhost:3000')
+app.listen(PORT, function () {
+	console.log(`Listening on http://localhost:${PORT}`)
 })
-
-const Pokemon = sequelize.define('pokemon', {
-	name: {
-		type: Sequelize.STRING,
-		allowNull: false
-	},
-	price: {
-		type: Sequelize.INTEGER,
-		allowNull: false
-	},
-	stock: {
-		type: Sequelize.INTEGER,
-		allowNull: true,
-		defaultValue: 1
-	}
-})
-
-Pokemon.sync({force: true}).then(function () {
-	console.log('Model is ready!')
-})
-
-app.get('/get-pokemons', function (req, res) {
-	Pokemon.findAll()
-		.then(function listOfPokemons(pokemons){
-			res.send(pokemons)
-		})
-})
-
-app.put('/create-pokemons', function (req, res) {
-	Pokemon.create(req.body)
-		.then(function sendPokemon(pokemon){
-			res.send(pokemon)
-		})
-})
-
-app.post('/buy-pokemons', function (req, res) {
-	Pokemon.findOne({
-		where: {
-			name: req.body.name
-		}
-	})
-	.then(function(pokemon) {
-		if (pokemon.stock < req.body.quantity) {
-			return res.status(400).send({
-				error: 'Not enought ' + pokemon.name + ' in stock: ' + pokemon.stock
-			})
-		}
-
-		request({
-			uri: 'https://api.pagar.me/1/transactions',
-			method: 'POST',
-			json: {
-				api_key: "******",
-				amount: pokemon.price * req.body.quantity * 100,
-				card_number: "4024007138010896",
-				card_expiration_date: "1050",
-				card_holder_name: "Ash Ketchum",
-				card_cvv: "123",
-				metadata: {
-					product: 'pokemon',
-					name: pokemon.name,
-					quantity: req.body.quantity
-				}
-			}
-		})
-		.then(function (body){
-			if (body.status == 'paid') {
-
-				pokemon.stock = pokemon.stock - req.body.quantity
-				pokemon.save()
-					.then(function(pokemon) {
-						res.send(body)
-					})
-			}
-		})
-		.catch(function (err){
-			console.log(JSON.stringify(err, null, 4))
-			return res.status(err.response.statusCode).send(err.response.body)
-		})
-
-	})
-
-})
-
-
