010369ea539a1f3d27ea0a6515cc8786d458ae0e
Otávio Pace <otaviopp8@gmail.com>
2017-08-19 11:19:42 -0300

    Adição do cors como dependência e nova middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,9 @@
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 function applyMiddleware (expressApp) {
     expressApp.use(bodyParser.json())
+    expressApp.use(cors())
 }
 
 module.exports = applyMiddleware
