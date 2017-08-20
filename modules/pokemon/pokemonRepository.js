dfd7e7cc651a2f1731d7e19a4c5f6a3843621a05
Otávio Pace <otaviopp8@gmail.com>
2017-08-20 08:44:13 -0300

    Adição de query de pokemons no endpoint de buscar pokemons

--- modules/pokemon/pokemonRepository.js
+++ modules/pokemon/pokemonRepository.js
@@ -1,7 +1,29 @@
 const Pokemon = require('../../models/pokemon')
 
-exports.getPokemon = () => {
-    return Pokemon.findAll()
+exports.getPokemon = (query = {}) => {
+    let pokemonQuery = {
+        where: { 
+            $and: [
+                {name: { $like: query.name ? `%${query.name}%` : '%%' }},
+                {price: { $eq: query.price }},
+                {stock: { $eq: query.stock }}
+            ]
+        }
+    }
+
+    if (!query.price) {
+        let priceObject = pokemonQuery.where.$and.find(obj => obj.price)
+        let indexOfPriceObject = pokemonQuery.where.$and.indexOf(priceObject)
+        pokemonQuery.where.$and.splice(indexOfPriceObject)
+    }
+
+    if (!query.stock) {
+        let stockObject = pokemonQuery.where.$and.find(obj => obj.stock)
+        let indexOfStockObject = pokemonQuery.where.$and.indexOf(stockObject)
+        pokemonQuery.where.$and.splice(indexOfStockObject)
+    }
+
+    return Pokemon.findAll(pokemonQuery)
 }
 
 exports.createPokemon = (pokemon) => {
