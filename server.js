833bd289faf46c8ddf24487fc67b1c4ebc698ae7
Otávio Pace <otaviopp8@gmail.com>
2017-08-20 08:42:59 -0300

    Remoção de dependência não mais utilizada no server.js(request-promise)

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const app = express()
-const request = require('request-promise')
 const applyMiddleware = require('./middleware')
 const syncModels = require('./models')
 const modules = require('./modules')
