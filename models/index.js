64fba6eb713743b1e013ff9db4b54c2878df9342
Otávio Pace <otaviopp8@gmail.com>
2017-08-23 01:00:58 -0300

    Mudança de sqlite para mysql, e reestruturação da forma como as models e conexão ao banco é feita. Adição do deploy.sh

--- models/index.js
+++ models/index.js
@@ -1,24 +1,32 @@
 const glob = require('glob')
 const path = require('path')
 
+const db = {}
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+	pool: {
+		maxIdleTime: 30000
+	},
+    host: process.env.DATABASE_HOST,
+    port: 3306,
+    dialect: 'mysql'
+})
+
 function syncModels () {
     glob.sync('./models/*.js', { ignore: './**/index.js' })
-        .forEach(filePath => {                
-            const model = require(path.resolve(filePath))
-            model.sync({force: true})
-                .then(() => {
-                    console.log(`${filePath.replace('./models/', '')
-                                .replace('.js', '')
-                                .toUpperCase()} model is ready!`)
-                })
-                .catch(error => {
-                    console.log(`Error on loading
-                                ${filePath.replace('./models/', '')
-                                .replace('.js', '')
-                                .toUpperCase()} model.
-                                ${error}`)
-                })
+        .forEach(filePath => {
+            const model = sequelize.import(path.resolve(filePath))
+            
+            let modelName = filePath.replace('.js', '').replace('./models/', '')
+            modelName = modelName.charAt(0).toUpperCase() + modelName.slice(1)
+
+            db[modelName] = model
         })
 }
 
-module.exports = syncModels
+db.syncModels = syncModels
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
